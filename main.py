6a9cbbf657bb2047737cb2bb5fb9db2164dc5510
Tom Reed <tomreed81@gmail.com>
2019-04-30 09:59:03 -0700

    Working integer array counter

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-# Python seed
+# Green Dot 2nd interview question
+# Count the number of times a number occurs in an array
+
+array = [1,2,3,4,1,2,3,4,1,2]
 
 def output(message):
     print message
 
 if __name__ == '__main__':
-    output("Hello world")
+    output("Array : " + str(array))
+
+    counts = {}
+
+    for element in array:
+        if counts.__contains__(element):
+            counts[element] += 1
+        else:
+            counts[element] = 1
+
+    output("Count : " + str(counts))
